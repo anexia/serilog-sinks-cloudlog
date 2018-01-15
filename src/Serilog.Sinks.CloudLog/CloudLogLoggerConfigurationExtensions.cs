3427764349f29cc5db6de3a9ef29aaa16d14b183
Michael Rauter <mrauter@anexia-it.com>
2018-01-15 13:38:42 +0100

    Initial commit

---
+++ src/Serilog.Sinks.CloudLog/CloudLogLoggerConfigurationExtensions.cs
@@ -0,0 +1,53 @@
+﻿using Serilog.Configuration;
+using Serilog.Sinks.CloudLog;
+
+namespace Serilog
+{
+    /// <summary>
+    ///     Adds the WriteTo.CloudLog() extension method to <see cref="LoggerConfiguration" />.
+    /// </summary>
+    public static class LoggerConfigurationKafkaExtensions
+    {
+        /// <summary>
+        /// Adds a sink that writes log events to a CloudLog index.
+        /// </summary>
+        /// <param name="loggerConfiguration">The logger configuration.</param>
+        /// <param name="index">The index name.</param>
+        /// <param name="caFile">The path of the CloudLog CA certificate.</param>
+        /// <param name="certFile">The path of the CloudLog client certificate.</param>
+        /// <param name="keyFile">The path of the CloudLog client key.</param>
+        /// <returns></returns>
+        public static LoggerConfiguration CloudLog(
+            this LoggerSinkConfiguration loggerConfiguration,
+            string index,
+            string caFile,
+            string certFile,
+            string keyFile)
+        {
+            var sink = new CloudLogSink(
+                index,
+                caFile,
+                certFile,
+                keyFile);
+            return loggerConfiguration.Sink(sink);
+        }
+
+        /// <summary>
+        /// Adds a sink that writes log events to a CloudLog index (HTTP interface).
+        /// </summary>
+        /// <param name="loggerConfiguration">The logger configuration.</param>
+        /// <param name="index">The index name.</param>
+        /// <param name="token">The CloudLog index token.</param>
+        /// <returns></returns>
+        public static LoggerConfiguration CloudLog(
+            this LoggerSinkConfiguration loggerConfiguration,
+            string index,
+            string token)
+        {
+            var sink = new CloudLogSink(
+                index,
+                token);
+            return loggerConfiguration.Sink(sink);
+        }
+    }
+}
---
+++ src/Serilog.Sinks.CloudLog/Sinks/CloudLogSink.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Serilog.Events;
+using Serilog.Formatting.Json;
+using Serilog.Sinks.PeriodicBatching;
+using Anexia.BDP.CloudLog;
+using System.IO;
+
+namespace Serilog.Sinks.CloudLog
+{
+    public class CloudLogSink : PeriodicBatchingSink
+    {
+        private Client client;
+        private JsonFormatter formatter;
+
+        const int batchSize = 50;
+        const int period = 1;
+
+        public CloudLogSink(
+            string index,
+            string caFile,
+            string certFile,
+            string keyFile) : base(batchSize, TimeSpan.FromSeconds(period))
+        {
+            this.client = new Client(index, caFile, certFile, keyFile);
+            this.formatter = new JsonFormatter(closingDelimiter: null, renderMessage: true);
+        }
+
+        public CloudLogSink(
+            string index,
+            string token) : base(batchSize, TimeSpan.FromSeconds(period))
+        {
+            this.client = new Client(index, token);
+            this.formatter = new JsonFormatter(closingDelimiter: null, renderMessage: true);
+        }
+
+        protected override void EmitBatch(IEnumerable<LogEvent> events)
+        {
+            foreach (var e in events)
+            {
+                var sw = new StringWriter();
+                formatter.Format(e, sw);
+                client.PushEvent(sw.ToString());
+            }
+        }
+    }
+}
