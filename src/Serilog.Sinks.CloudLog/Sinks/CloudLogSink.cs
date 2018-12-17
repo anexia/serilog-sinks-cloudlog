7a7340e7e061208cd90f0d89bd1ecd432e28a123
Dan <dan@beyondscalability.com>
2018-12-17 13:15:40 +0100

    follow serilog reliability guidelines

--- src/Serilog.Sinks.CloudLog/Sinks/CloudLogSink.cs
+++ src/Serilog.Sinks.CloudLog/Sinks/CloudLogSink.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Serilog.Events;
 using Serilog.Formatting.Json;
 using Serilog.Sinks.PeriodicBatching;
+using Serilog.Debugging;
 using Anexia.BDP.CloudLog;
 using System.IO;
 using Serilog.Sinks.CloudLog.Formatting;
@@ -17,29 +18,36 @@ namespace Serilog.Sinks.CloudLog
         const int batchSize = 50;
         const int period = 1;
 
-        public CloudLogSink(
-            string index,
-            string caFile,
-            string certFile,
-            string keyFile,
-            string keyPassword="") : base(batchSize, TimeSpan.FromSeconds(period))
+        public CloudLogSink(string index, string caFile, string certFile, string keyFile, string keyPassword = "") : base(batchSize, TimeSpan.FromSeconds(period))
         {
-            this.client = new Client(index, caFile, certFile, keyFile, keyPassword);
-            Init();
+            try
+            {
+                client = new Client(index, caFile, certFile, keyFile, keyPassword);
+                Init();
+            }
+            catch (Exception exception)
+            {
+                SelfLog.WriteLine("Unable to create CloudLogSink for index {0}: {1}", index, exception);
+            }
         }
 
-        public CloudLogSink(
-            string index,
-            string token) : base(batchSize, TimeSpan.FromSeconds(period))
+        public CloudLogSink(string index, string token) : base(batchSize, TimeSpan.FromSeconds(period))
         {
-            this.client = new Client(index, token);
-            Init();
+            try
+            {
+                client = new Client(index, token);
+                Init();
+            }
+            catch (Exception exception)
+            {
+                SelfLog.WriteLine("Unable to create CloudLogSink for index {0}: {1}", index, exception);
+            }
         }
 
         private void Init()
         {
-            this.formatter = new CloudLogJsonFormatter();
-            this.client.SetClientType("dotnet-client-serilog");
+            formatter = new CloudLogJsonFormatter();
+            client.SetClientType("dotnet-client-serilog");
         }
 
         protected override void EmitBatch(IEnumerable<LogEvent> events)
